a139e994b706e852a6ba3d564370e801766ddae7
Mr. Pi contact@mr-pi.de <mrpi@mr-pi.de>
2015-02-05 16:26:12 +0100

    functions to display angels

---
+++ dispdeg.m
@@ -0,0 +1,22 @@
+function [] = dispdeg(varargin)
+%DISPDEG  displays variable names and their values(converted to degree)
+%   a format string can be provided as first argument
+%   ex. dispdeg('%9.6f', a, b, c);
+%
+%   © 2015 - Markus Mr. <github-contact@mr-pi.de>
+	format_str = '%6.3fdeg';
+
+	start = isstr(varargin{1})+1; %if first argument a string, it is the format string
+
+	if start==2
+		format_str=varargin{1};
+	end
+	
+	for i = start:nargin-1
+		fprintf(['%12s: ',format_str,', '], inputname(i), rad2gon(varargin{i}));
+		if mod(i,4)==0
+			fprintf('\n')
+		end
+	end
+	fprintf(['%12s: ',format_str,'\n'], inputname(nargin), rad2gon(varargin{nargin}));
+end
---
+++ dispgon.m
@@ -0,0 +1,22 @@
+function [] = dispgon(varargin)
+%DISPGON  displays variable names and their values(converted to gons)
+%   a format string can be provided as first argument
+%   ex. dispgon('%9.6f', a, b, c);
+%
+%   © 2015 - Markus Mr. <github-contact@mr-pi.de>
+	format_str = '%6.3fgon';
+
+	start = isstr(varargin{1})+1; %if first argument a string, it is the format string
+
+	if start==2
+		format_str=varargin{1};
+	end
+	
+	for i = start:nargin-1
+		fprintf(['%12s: ',format_str,', '], inputname(i), varargin{i});
+		if mod(i,4)==0
+			fprintf('\n')
+		end
+	end
+	fprintf(['%12s: ',format_str,'\n'], inputname(nargin), varargin{nargin});
+end
