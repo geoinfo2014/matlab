98a75eaa0c594d58d0d22bfb34e3185be17873fc
Markus Mr <github-contact@mr-pi.de>
2015-05-06 20:34:20 +0200

    Nice point plot function

---
+++ plotPoints.m
@@ -0,0 +1,25 @@
+function [pointsX] = plotPoints(varargin)
+%PLOTPOINTS  plots given points ant there names at the axes
+%   the points shoult provide as x1,y1,x2,y2,…,xN,yN
+%   ex. plotPoints(x1,y1,x2,y2);
+%
+%   © 2015 - Markus Mr. <github-contact@mr-pi.de>
+
+	for i = 1:nargin/2
+		pointsX{i,1}=varargin{i*2-1};
+		pointsX{i,2}=inputname(i*2-1);
+		pointsY{i,1}=varargin{i*2};
+		pointsY{i,2}=inputname(i*2);
+	end
+	pointsX=sortrows(pointsX,1);
+	pointsY=sortrows(pointsY,1);
+
+	figure();
+	hold();
+	grid on;
+	set(gca,'XTick',cell2mat(pointsX(:,1)));
+	set(gca,'YTick',cell2mat(pointsY(:,1)));
+	set(gca,'XTickLabel',str2mat(pointsX(:,2)));
+	set(gca,'YTickLabel',str2mat(pointsY(:,2)));
+	plot(cell2mat(pointsX(:,1)),cell2mat(pointsY(:,1)),'*');
+end
